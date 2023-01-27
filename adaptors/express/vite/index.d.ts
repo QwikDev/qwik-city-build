931660d89964b6d94e1cfd23cd1f116b08da66d4
Manu MA <manu.mtza@gmail.com>
2023-01-27 12:16:35 +0000

    2701393 feat: add className util
    https://github.com/BuilderIO/qwik/commit/699d5956314ab6f431d7db3d762724218db643eb

---
+++ adaptors/express/vite/index.d.ts
@@ -0,0 +1,14 @@
+import { ServerAdaptorOptions } from '../../shared/vite';
+
+/**
+ * @alpha
+ */
+export declare function expressAdaptor(opts?: ExpressAdaptorOptions): any;
+
+/**
+ * @alpha
+ */
+export declare interface ExpressAdaptorOptions extends ServerAdaptorOptions {
+}
+
+export { }
