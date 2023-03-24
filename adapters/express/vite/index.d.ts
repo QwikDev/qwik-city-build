61efcec83dc4ee33f69f5064ff7e36317f02c7b8
Manu MA <manu.mtza@gmail.com>
2023-03-24 09:37:14 +0000

    b33df31 fix: deprecate express adapter (#3500)
    https://github.com/BuilderIO/qwik/commit/27c297ce572dbc9194223ba4788fc40c1fe05c91

--- adapters/express/vite/index.d.ts
+++ adapters/express/vite/index.d.ts
@@ -3,24 +3,26 @@ import type { StaticGenerateRenderOptions } from '@builder.io/qwik-city/static';
 
 /**
  * @public
+ * @deprecated - Use `nodeServerAdapter` exported from `@builder.io/qwik-city/adapters/node-server/vite` instead.
  */
 export declare function expressAdapter(opts?: ExpressAdapterOptions): any;
 
 /**
  * @public
+ * @deprecated - Use `NodeServerAdapterOptions` exported from `@builder.io/qwik-city/adapters/node-server/vite` instead.
  */
 export declare interface ExpressAdapterOptions extends ServerAdapterOptions {
 }
 
 /**
  * @public
- * @deprecated Use `expressAdapter` exported from `@builder.io/qwik-city/adapters/express/vite` instead.
+ * @deprecated - Use `nodeServerAdapter` exported from `@builder.io/qwik-city/adapters/node-server/vite` instead.
  */
 export declare const expressAdaptor: typeof expressAdapter;
 
 /**
  * @public
- * @deprecated Use `ExpressAdapterOptions` instead.
+ * @deprecated - Use `NodeServerAdapterOptions` exported from `@builder.io/qwik-city/adapters/node-server/vite` instead.
  */
 export declare type ExpressAdaptorOptions = ExpressAdapterOptions;
 
