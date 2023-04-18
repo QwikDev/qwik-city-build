205a0294bf16bd61d43578cf96cd0bdbc1149522
Manu MA <manu.mtza@gmail.com>
2023-04-18 13:50:42 +0000

    dca00ca feat: serialize server$ exceptions (#3821)
    https://github.com/BuilderIO/qwik/commit/78a678c1d6421031462720266d74f7382b6037ce

--- middleware/request-handler/index.mjs
+++ middleware/request-handler/index.mjs
@@ -410,7 +410,14 @@ async function pureServerFunction(ev) {
     if (Array.isArray(data)) {
       const [qrl, ...args] = data;
       if (isQrl(qrl) && qrl.getHash() === fn) {
-        const result = await qrl.apply(ev, args);
+        let result;
+        try {
+          result = await qrl.apply(ev, args);
+        } catch (err) {
+          ev.headers.set("Content-Type", "application/qwik-json");
+          ev.send(500, await qwikSerializer._serializeData(err, true));
+          return;
+        }
         if (isAsyncIterator(result)) {
           ev.headers.set("Content-Type", "text/event-stream");
           const stream = ev.getWritableStream().getWriter();
@@ -418,7 +425,6 @@ async function pureServerFunction(ev) {
             verifySerializable(qwikSerializer, item, qrl);
             ev.headers.set("Content-Type", "application/qwik-json");
             const message = await qwikSerializer._serializeData(item, true);
-            verifySerializable(qwikSerializer, result, qrl);
             stream.write(encoder.encode(`event: qwik
 data: ${message}
 
@@ -426,8 +432,10 @@ data: ${message}
           }
           stream.close();
         } else {
+          verifySerializable(qwikSerializer, result, qrl);
           ev.headers.set("Content-Type", "application/qwik-json");
-          ev.send(200, await qwikSerializer._serializeData(result, true));
+          const message = await qwikSerializer._serializeData(result, true);
+          ev.send(200, message);
         }
         return;
       }
