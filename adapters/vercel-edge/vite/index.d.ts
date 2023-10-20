7608dd4443873396da36c9fee0ba5bd9a4c4e830
D <intellix@users.noreply.github.com>
2023-10-20 08:22:37 +0000

    f1b69ec feat: add outDir support to qwikInsights (#5335)
    https://github.com/BuilderIO/qwik/commit/2ef8af9f06b086b6c23a7846fddb11df8abbbc8c

--- adapters/vercel-edge/vite/index.d.ts
+++ adapters/vercel-edge/vite/index.d.ts
@@ -37,8 +37,8 @@ export declare interface VercelEdgeAdapterOptions extends ServerAdapterOptions {
     /**
      * Define the `target` proeprty in the `ssr` property in the `vite.config.ts` file.
      *
-     * Defaults to `webworker` for not having a breaking change. But `node` will become the default
-     * in an upcoming release.
+     * Defaults to `webworker` for not having a breaking change. But `node` will become the default in
+     * an upcoming release.
      */
     target?: 'webworker' | 'node';
 }
