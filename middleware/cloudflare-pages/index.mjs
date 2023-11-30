deb5b1634c8818615b7d859877a9e48661e5bc78
Marcos Perona <mperona@gmail.com>
2023-11-30 21:05:37 +0000

    217f58c fix: cf pages polyfill only if needed (#5507)
    https://github.com/BuilderIO/qwik/commit/6d901669b69559433612a43a6483e4bddace6431

--- middleware/cloudflare-pages/index.mjs
+++ middleware/cloudflare-pages/index.mjs
@@ -8,7 +8,9 @@ import { isStaticPath } from "@qwik-city-static-paths";
 import { _deserializeData, _serializeData, _verifySerializable } from "@builder.io/qwik";
 import { setServerPlatform } from "@builder.io/qwik/server";
 function createQwikCity(opts) {
-  globalThis.TextEncoderStream = TextEncoderStream;
+  if (typeof globalThis.TextEncoderStream === "undefined") {
+    globalThis.TextEncoderStream = TextEncoderStream;
+  }
   const qwikSerializer = {
     _deserializeData,
     _serializeData,
