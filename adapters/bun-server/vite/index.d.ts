bf7e213ed2216d87096964f1ca4cc54b4f604c77
Miško Hevery <misko@hevery.com>
2023-12-08 21:41:42 +0000

    78c2c11 feat(qwik): Experimental support for synchronous QRL `$sync()`.
    https://github.com/BuilderIO/qwik/commit/726498adfb4ea756728e2944c68992ddaa4ec006

---
+++ adapters/bun-server/vite/index.d.ts
@@ -0,0 +1,14 @@
+import { ServerAdapterOptions } from '../../shared/vite';
+import type { StaticGenerateRenderOptions } from '@builder.io/qwik-city/static';
+
+/** @alpha */
+export declare function bunServerAdapter(opts?: bunServerAdapterOptions): any;
+
+/** @alpha */
+export declare interface bunServerAdapterOptions extends ServerAdapterOptions {
+    name?: string;
+}
+
+export { StaticGenerateRenderOptions }
+
+export { }
